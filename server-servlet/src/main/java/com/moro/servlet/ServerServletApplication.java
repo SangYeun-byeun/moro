5b284dd7b06021e6fbc1e9a755f84af698c64bf4
LAPTOP-G8SCA2D7\zoroq <zoroqp@gmail.com>
2022-03-08 12:07:38 +0900

    moro

---
+++ server-servlet/src/main/java/com/moro/servlet/ServerServletApplication.java
@@ -0,0 +1,13 @@
+package com.moro.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerServletApplication.class, args);
+    }
+
+}
---
+++ server-servlet/src/test/java/com/moro/servlet/ServerServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moro.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerServletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
